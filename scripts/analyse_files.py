dfdc8df43eb9d510d67588f7c995bb276e35ecee
Serj <serj2work@gmail.com>
2023-06-27 22:04:01 +0100

    Added some error checks

--- scripts/analyse_files.py
+++ scripts/analyse_files.py
@@ -6,8 +6,22 @@ from auto_shot_list.scene_manager import VideoManager
 
 
 def analyse_files(videos_dir: Path, output_dir: Path):
+    """
+    Analyses all files in given folder
+    :param videos_dir: path to a directory with video files
+    :param output_dir: path to a directory to store created shot lists
+    :return:
+    """
 
     analyser = OpenAIFrameAnalyser()
+
+    if not videos_dir.exists():
+        raise ValueError(f"Videos directory does not exists: {videos_dir}")
+
+    if not output_dir.exists():
+        raise ValueError(f"Output directory does not exists: {videos_dir}")
+
+
 
     for video_path in videos_dir.iterdir():
         video_manager = VideoManager(str(video_path), frame_analyser=analyser)
