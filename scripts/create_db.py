8dd13fe6be53b63f13a03c831fbbded5c4fdf22d
Serj <serj2work@gmail.com>
2023-07-02 13:24:31 +0100

    Set the video path in metadata

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,8 +1,10 @@
 import argparse
 import json_tricks
+import logging
 from pathlib import Path
 import pandas as pd
 from uuid import uuid1
+from auto_shot_list.scene_manager import VideoManager
 from langchain.embeddings import OpenAIEmbeddings, LlamaCppEmbeddings
 from langchain.vectorstores import Chroma
 
@@ -20,11 +22,11 @@ def process_shots(shot_list_dir: Path, result_dir: Path):
 
     for shot_list_path in shot_list_dir.iterdir():
         with open(shot_list_path, "r") as f:
-            data = json_tricks.load(f)
+            data: VideoManager = json_tricks.load(f)
         for shot in data.scenes_description:
             shot_id = str(uuid1())
             metadatas.append({
-                "path": shot_list_path,
+                "path": data.video_path,
                 "frame_start": shot["timing"][0].frame_num,
                 "frame_end": shot["timing"][1].frame_num,
                 "duration": shot["timing"][1].get_seconds() - shot["timing"][0].get_seconds()
